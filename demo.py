a7b781bd35d72e0f76401386ea7801b89aa973d3
afaguayo <afaguayo@miners.utep.edu>
2024-11-09 16:16:47 -0700

    demo

---
+++ demo.py
@@ -0,0 +1,20 @@
+import gym
+import minerl
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+env = gym.make('MineRLBasaltFindCave-v0')
+
+obs = env.reset()
+
+
+done = False
+
+while not done:
+    # Take a random action
+    action = env.action_space.sample()
+    # In BASALT environments, sending ESC action will end the episode
+    # Lets not do that
+    action["ESC"] = 0
+    obs, reward, done, _ = env.step(action)
+    env.render()
